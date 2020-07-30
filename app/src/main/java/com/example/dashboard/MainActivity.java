e205e51e363ec8284397a2275f863306f0e36148
Raghwani PC <mitaliraghwani@gmail.com>
2020-07-30 20:17:01 +0530

    Login single screen

---
+++ app/src/main/java/com/example/dashboard/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dashboard;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hanks.passcodeview.PasscodeView;
+
+public class MainActivity extends AppCompatActivity {
+    //initialize variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
